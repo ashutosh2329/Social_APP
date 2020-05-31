a4f91501c703a01412b32e1f5a3b926429521a91
ashutosh2329 <sonutiwari2329@gmail.com>
2020-05-31 20:56:43 +0530

    templates

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -4,7 +4,8 @@ from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Hellow world</h1>")
+	return render(request,"pages/home.html",context={},status=200)
+	# return HttpResponse("<h1>Hellow world</h1>")
 
 def tweet_detail_view(request,tweet_id, *args, **kwargs):
 	"""
@@ -26,4 +27,6 @@ def tweet_detail_view(request,tweet_id, *args, **kwargs):
 		data['message'] = "Not Found"
 		status = 400
 
-	return JsonResponse(data, status=status)
+	return render(request,"pages/tweets.html",context=data)
+
+	# return JsonResponse(data, status=status)
