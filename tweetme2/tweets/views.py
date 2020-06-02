c301a7e7531f4bba57572a8b42a2347b6d1e132f
ashutosh2329 <sonutiwari2329@gmail.com>
2020-06-02 20:40:17 +0530

    js included

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -7,12 +7,26 @@ def home_view(request, *args, **kwargs):
 	return render(request,"pages/home.html",context={},status=200)
 	# return HttpResponse("<h1>Hellow world</h1>")
 
+
+def tweet_list_view(request, *args, **kwargs):
+	"""
+	Rest Api VIEW
+	consume by javascript
+	"""
+	querry_set = Tweet.objects.all()
+	tweets_list = [{"id":x.id, "title":x.title, "content": x.content} for x in querry_set]
+	data ={
+		"isUser": False,
+		"response":tweets_list,
+	}
+	return JsonResponse(data)
+
+
+
 def tweet_detail_view(request,tweet_id, *args, **kwargs):
 	"""
 	Rest Api VIEW
 	consume by javascript
-
-
 	"""
 	data ={
 	     "id": tweet_id,
